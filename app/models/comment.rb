f0cf0001eb969bcc5db0182730787c661c890ac5
lbmaxwell <lbmaxwell@gmail.com>
2012-06-13 15:09:06 -0400

    Comment length validation.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :comment, :time_entry, :user
 
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { minimum: 1, maximum: 255 }
 end
