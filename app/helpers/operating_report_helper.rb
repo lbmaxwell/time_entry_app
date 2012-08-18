2a5dc54c5b6721daa7eefdd1f7e6fe7f00902601
percheron <bmaxwell@mdenergy.com>
2012-08-18 11:41:30 -0400

    Update database configurations

--- app/helpers/operating_report_helper.rb
+++ app/helpers/operating_report_helper.rb
@@ -43,7 +43,7 @@ module OperatingReportHelper
   def table_data(is_daily = false)
     table_data = Hash.new(0)
     tasks = tasks_for_operating_report
-    time_entries_local = time_entries
+    time_entries = get_time_entries
     total_volume = Hash.new(0)
     total_time = Hash.new(0)
 
@@ -82,7 +82,7 @@ module OperatingReportHelper
         seconds = 0
         count = 0
 
-        time_entries_local.each do |time_entry|
+        time_entries.each do |time_entry|
           if is_daily
             if time_entry.task == task && time_entry.effective_date == date
               if task.is_direct?
@@ -147,9 +147,10 @@ module OperatingReportHelper
   end # def table_data
 
   def table_data_print(is_daily = false)
-    table_data = Hash.new(0)
+    time_entries = get_time_entries
     tasks = tasks_for_operating_report
-    time_entries_local = time_entries
+
+    table_data = Hash.new
     total_volume = Hash.new(0)
     total_time = Hash.new(0)
 
@@ -162,97 +163,13 @@ module OperatingReportHelper
       date_range = last_days_of_week_for_date_range(start_date, end_date)
     end
 
-    date_range.each do |date|
-      if is_daily
-        regular_paid_time_entries = get_paid_time_entries(date, date, false)
-        overtime_paid_time_entries = get_paid_time_entries(date, date, true)
-      else
-        regular_paid_time_entries = get_paid_time_entries(date - 7, date, false)
-        overtime_paid_time_entries = get_paid_time_entries(date - 7, date, true)
-      end
-
-      earned_seconds = 0
-      nested_hash = Hash.new
-
-      nested_hash[:reg_minutes] = time_entries_minute_total(regular_paid_time_entries)
-      nested_hash[:ot_minutes] = time_entries_minute_total(overtime_paid_time_entries)
-      nested_hash[:total_minutes] = nested_hash[:reg_minutes] + nested_hash[:ot_minutes]
-
-      total_time[:reg_minutes] += nested_hash[:reg_minutes]
-      total_time[:ot_minutes] += nested_hash[:ot_minutes]
-
-      indirect_seconds = 0
-
-      tasks.each do |task|
-
-        seconds = 0
-        count = 0
-
-        time_entries_local.each do |time_entry|
-          if is_daily
-            if time_entry.task == task && time_entry.effective_date == date
-              if task.is_direct?
-                count += time_entry.number_processed
-                seconds += time_entry.seconds * time_entry.number_processed
-              else
-                indirect_seconds += time_entry.seconds
-                seconds += time_entry.seconds
-              end
-            end
-          else
-            if (time_entry.task == task && 
-                  last_day_in_week(time_entry.effective_date) == date)
-              if task.is_direct?
-                count += time_entry.number_processed
-                seconds += time_entry.seconds * time_entry.number_processed
-              else
-                indirect_seconds += time_entry.seconds
-                seconds += time_entry.seconds
-              end
-            end
-          end
-        end # |time_entry|
-
-        if task.is_direct?
-          nested_hash[task.id] = count
-          total_volume[task.id] += count
-          earned_seconds += seconds
-        else
-          nested_hash[task.id] = seconds_to_hours(seconds)
-        end
-        total_time[task.id] += seconds
-      end # |task|
-
-      nested_hash[:actual_direct_seconds] = ((nested_hash[:total_minutes] * 60) - 
-          indirect_seconds)
-
-      if nested_hash[:total_minutes] != 0
-        nested_hash[:utilization] = ((nested_hash[:actual_direct_seconds].to_f / 
-            (nested_hash[:total_minutes].to_f * 60)) * 100).round(1).to_s + '%'
-
-        nested_hash[:production] = ((earned_seconds.to_f / 
-            (nested_hash[:total_minutes].to_f * 60)) * 100).round(1).to_s + '%'
-      else
-        nested_hash[:utilization] = '-'
-        nested_hash[:production] = '-'
-      end
-
-      if nested_hash[:actual_direct_seconds] <= 0 
-        nested_hash[:efficiency] = '-'
-      else
-        nested_hash[:efficiency] = ((earned_seconds.to_f / 
-            nested_hash[:actual_direct_seconds].to_f) * 100).round(1).to_s + '%'
-      end
-
-      table_data[date] = nested_hash
-      table_data[:actual_direct_seconds_total] += nested_hash[:actual_direct_seconds]
-    end # |date|
-    table_data[:total_volume] = total_volume
-    table_data[:total_time] = total_time
-    return table_data
+    tasks.each do |task|
+      date_range.each do |date|
+      end # date_range.each do |date|
+    end # tasks.each do |task|
   end # def table_data_print
 
-  def time_entries
+  def get_time_entries
     if current_user.admin?
       if params[:users].nil? || params[:users].first == ''
         @time_entries = TimeEntry.where(team_id: params[:team],
